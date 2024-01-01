06f5df7e5f34c6ee5c6f9f60c0402fc604435501
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-01-01 20:50:53 +0200

    solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (typeof start !== 'number' || typeof end !== 'number') return 'ERROR';
 
+  if (start < 0 || end < 0) return 'ERROR';
+
+  if (start > end) [start, end] = [end, start];
+
+  let totalSum = 0;
+  for (let i = start; i <= end; i++) {
+    totalSum += i;
+  }
+  return totalSum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
