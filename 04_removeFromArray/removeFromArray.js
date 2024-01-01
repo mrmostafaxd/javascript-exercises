1d7e1048a87bd7ea6c4db5ea4a561e891ae6a7cb
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-01-01 20:33:25 +0200

    solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...elementsToBeDeleted) {
+  const newArray = [];
+  inputArray.forEach((el) => {
+    if (!elementsToBeDeleted.includes(el)) newArray.push(el);
+  });
+  return newArray;
 };
 
 // Do not edit below this line
