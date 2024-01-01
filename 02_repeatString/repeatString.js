7541c0af6751067ef4651c9418049f04133ca2ba
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-01-01 20:06:20 +0200

    solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, noOfRepeats) {
+  if (noOfRepeats < 0) return 'ERROR';
 
+  let resultString = '';
+  for (let i = 0; i < noOfRepeats; i++) {
+    resultString += str;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
