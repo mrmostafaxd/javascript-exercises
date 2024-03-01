a482e70619f6372cee03d9f5b178f1c29447d236
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-03-01 23:52:40 +0200

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const textCleaned = text.replace(/[\.,?!\s]/gi, '').toLowerCase();
 
+  for (let i = 0; i < textCleaned.length; i++) {
+    if (textCleaned[i] !== textCleaned[textCleaned.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
