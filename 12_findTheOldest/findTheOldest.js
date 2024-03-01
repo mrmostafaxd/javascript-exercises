f38382d68f83b1660fc9dd083e9f3ef30430a5e5
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-03-02 00:11:07 +0200

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people
+    .map((person) => {
+      const personCopy = { ...person };
+      if (!('yearOfDeath' in personCopy)) {
+        personCopy.yearOfDeath = new Date().getFullYear();
+      }
+      return {
+        name: personCopy.name,
+        age: personCopy.yearOfDeath - personCopy.yearOfBirth,
+      };
+    })
+    .sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
